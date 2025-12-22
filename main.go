651fcaf11b0df046793ed40ff667d0c4981e90d7
wbhemingway <william.b.hemingway@gmail.com>
2025-12-22 15:14:14 -0500

    added browse

--- main.go
+++ main.go
@@ -47,6 +47,7 @@ func main() {
 	cmds.register("follow", middlewareLoggedIn(handlerFollowFeed))
 	cmds.register("following", middlewareLoggedIn(handlerFollowingFeeds))
 	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollowFeed))
+	cmds.register("browse", middlewareLoggedIn(handlerBrowse))
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
